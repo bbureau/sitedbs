0486832dfa8dcaa656a5f983fedd8e9ee988f87a
Louis Milhac <louis.milhac@gmail.com>
2019-02-13 15:20:19 +0100

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -4,14 +4,41 @@
 		<meta charset="utf-8" />
 		<title> Page de contacts </title>
 	</head>
+
+	<body>
+		<header>
 	<?php include("header.php"); ?> <!-- inclus les liens visibles partout-->
+		</header>
+
+		<section>
 	<div style="background-color:lightblue">
 		<ul>
 			<li><a href="equipe.php"> Présentation de l'équipe </a></li>
 			<li><a href="Messagerie.php"> Messagerie </a></li>
 		</ul>
 	</div>
-	
+		</section>
+		
+		<section id="prez">
+			<figure>
+			Prez
+   			<img src="image.jpg" alt="Photo prez" title="photo prez!" />
+   			<figcaption> Julien Guinot </figcaption>
+   			Téléphone: xx xx xx xx xx
+   			email: julien.guinot@ecl18.ec-lyon.fr
+   			</figure>
+
+   		</section>"
+
+   		<section id="sg_matos">
+   			SG Matos
+   			<img src="image.jpg" alt="Photo prez" title="photo prez!" />
+   			<figcaption> Paul Flagel </figcaption>
+   			Téléphone: xx xx xx xx xx
+   			email: paul.flagel@ecl18.ec-lyon.fr
+   			</figure>
+   		</section>
+
 	
 	
 	
