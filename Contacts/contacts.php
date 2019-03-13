89b158fc9f967e1ff5490e4d7172ba7386251066
Louis Milhac <louis.milhac@gmail.com>
2019-03-13 15:28:33 +0100

    changement contact

--- Contacts/contacts.php
+++ Contacts/contacts.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
-		 <link rel="stylesheet" href="contact.css" />
+		 <link rel="stylesheet" href="contacts.css" />
 		<title> Page de contacts </title>
 	</head>
 
@@ -11,34 +11,34 @@
 	<?php include("header.php"); ?> <!-- inclus les liens visibles partout-->
 		</header>
 
-		<section>
-		<ul>
-			<li><a href="equipe.php"> Présentation de l'équipe </a></li>
-			<li><a href="Messagerie.php"> Messagerie </a></li>
-		</ul>
-	</div>
-		</section>
+
 		
-		<section id="prez">
-			<figure>
-			Prez
-   			<img src="Prez_DBS.jpg" alt="Photo prez" title="photo prez!" />
-   			<figcaption> Julien Guinot </figcaption>
-   			Téléphone: xx xx xx xx xx
-   			email: julien.guinot@ecl18.ec-lyon.fr
-   			</figure>
+		<article>
+
+			<section id="prez">
+				<figure>
+					Prez
+   					<img src="Prez_DBS.jpg" alt="Photo prez" title="photo prez!"  />
+   					<figcaption> Julien Guinot </figcaption>
+   					Téléphone: xx xx xx xx xx
+   					email: julien.guinot@ecl18.ec-lyon.fr
+   				</figure>
 
    		</section>
 
    		<section id="sg_matos">
-   			SG Matos
-   			<img src="SG_DBS.jpg" alt="Photo prez" title="photo prez!" />
-   			<figcaption> Paul Flagel </figcaption>
-   			Téléphone: xx xx xx xx xx
-   			email: paul.flagel@ecl18.ec-lyon.fr
+   			<figure>
+   				SG Matos
+   				<img src="SG_DBS.jpg" alt="Photo prez" title="photo prez!" />
+   				<figcaption> Paul Flagel </figcaption>
+   				Téléphone: xx xx xx xx xx
+   				email: paul.flagel@ecl18.ec-lyon.fr
    			</figure>
    		</section>
 
+   	</article>
+
+   	</body>
 	
 	
 	
